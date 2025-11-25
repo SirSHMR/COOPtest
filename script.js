abf552b8a97e737c115b0882bbd04f8b72f80f9f
SirSHMR <sirshmr11@gmail.com>
2025-11-26 00:27:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -126,10 +126,8 @@ async function loginUser() {
 
     if (data.user) {
       await resetAttempts(email);
-      const now = new Date().toLocaleString('en-SA', { timeZone: 'Asia/Riyadh' });
       const { data: rpcData, error: rpcError } = await supabase.rpc("record_success_login", {
-      user_email: email,
-      login_time: now
+      user_email: email
     });
 
   if (rpcError) console.error("RPC Error:", rpcError);
